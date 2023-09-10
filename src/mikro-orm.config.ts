2c793047c1bcfaf2d44f38226188d1ea3561c65c
Arturo Bernal <arturo@email.com>
2023-09-10 23:30:24 +0200

    bug

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -3,6 +3,7 @@ import {ConnectionOptions, LoadStrategy} from '@mikro-orm/core';
 export default {
     entities: ['dist/entities/*.js'], // compiled JS files
     entitiesTs: ['src/entities/*.ts'],
+    type: 'mariadb', // one of `mongo` | `mysql` | `mariadb` | `postgresql` | `sqlite`
     dbName: process.env.DB_DATABASE || 'sanvipop',
     user: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || '',
