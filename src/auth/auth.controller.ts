f0a548e7515ebfd87504270f04301f8924eee23d
Arturo Bernal <arturober@gmail.com>
2024-03-14 16:04:42 +0100

    error

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -50,6 +50,7 @@ export class AuthController {
     try {
       return await this.authService.loginGoogle(tokenDto);
     } catch (e) {
+      console.log(e);
       throw new UnauthorizedException(
         {
           status: HttpStatus.UNAUTHORIZED,
